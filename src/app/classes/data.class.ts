b6f2157ebcc11f77f2862587d1fb7ad85e7e2d32
Marcos Lage <mlage@ic.uff.br>
2017-07-18 14:53:23 -0300

    Scatter Plot improvements

--- src/app/classes/data.class.ts
+++ src/app/classes/data.class.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-
 import { Http } from '@angular/http';
 
 @Injectable()
@@ -7,10 +6,8 @@ export class DataService {
 
     private data : any;
 
-    constructor(private http: Http) {
-        
-    }
-
+    constructor(private http: Http) { }
+    
     getFeatures() {
         return this.http.get('./data/nyc/flickr-features.json').map((res:any) => res.json());
     }
--- src/app/classes/scatter.chart.class.ts
+++ src/app/classes/scatter.chart.class.ts
@@ -10,30 +10,32 @@ import * as _ from 'lodash';
 // my services
 import { DataService } from './data.class';
 
-export class ScatterChart{
+export class ScatterChart 
+{
     // margins setup
-    private margins: any = {top: 20, right: 20, bottom: 30, left: 50};
-    
-    private element: any;
+    private margins: any = { top: 5, right: 20, bottom: 40, left: 20 };
+    private spaceBetween: number = 10;
 
     // entire chart size
     private chartWidth: number = 0;
     private chartHeight: number = 0;
+    
     // each matrix size
     private elemWidth: number = 0;
     private elemHeight: number = 0;
+
+    // html element
+    private element: any;
 
     // dom elements
     private div: any;
     private svg: any;
     private cht: any;
-    private axiX: any;
-    private axiY: any;
-        
+
     // axis objects
     private xAxis: any;
     private yAxis: any;
-    
+
     private xRange: any;
     private yRange: any;
 
@@ -42,154 +44,270 @@ export class ScatterChart{
 
     private data: any;
     private timeRes: any[];
+    private nCharts: number;
 
-    private search: boolean;
-    
-    constructor(element: ElementRef, private dataService : DataService){
-        dataService.getFeatures().subscribe((json:any)=>{ 
+    private isSearch: boolean = false;
 
+    constructor(element: ElementRef, private dataService: DataService) 
+    {
+        dataService.getFeatures().subscribe((json: any) => 
+        {
+            // html element reference 
             this.element = element;
-            // sort features
-            var features = json.features.sort(function(x: any, y: any) {
-                return d3.ascending(x.rank, y.rank);
-            });
-            this.timeRes = Object.keys(features[features.length-1]["resolutions"]);
 
-            // remove ALL key
-            this.timeRes.splice(this.timeRes.indexOf("ALL"),1);
-
-            this.data = {};
-            this.xRange = [Infinity, -Infinity];
-            this.yRange = [Infinity, -Infinity];
-            var that = this;
-            this.timeRes.forEach(function(res) {
-                that.data[res] = features.map(function(f:any) {
-                    var resolution = res;
-                    var fnRank = f.resolutions[resolution].fnRank;
-                    var maxRank = f.resolutions[resolution].fnRank;
-                    var sigRank = f.resolutions[resolution].fnRank;
-                    var x = Math.sqrt(maxRank*maxRank + fnRank*fnRank + sigRank*sigRank);
-                    var y = f.rank;
-
-                    that.xRange[0] = Math.min(that.xRange[0], x);
-                    that.xRange[1] = Math.max(that.xRange[1], x);
-
-                    that.yRange[0] = Math.min(that.yRange[0], y);
-                    that.yRange[1] = Math.max(that.yRange[1], y);
-
-                    return {x: x, y: y};
-                });
-            });
+            // format data
+            this._buildData(json);
             
+            // update the chart
+            this.updateChart();
+        });
 
-            this._buildChart();
+        // Adds event listener resize when the window changes size.
+        window.addEventListener("resize", () => { this.updateChart() });            
+    }
 
-            // Adds event listener resize when the window changes size.
-            window.addEventListener("resize", () => { this._resize() });
+    updateChart()
+    {
+        // number of charts
+        this.nCharts = this.isSearch ? 1 : this.timeRes.length;
+
+        // dom elements
+        this._buildDomElems();
+        // build the axis
+        this._buildAxis()
+
+        // creates the chart
+        this._buildChart();        
+    }
+
+    private _buildDomElems()
+    {
+        // element div
+        this.div = d3.select(this.element.nativeElement);
+        
+        // chart size definition
+        this.chartWidth  = this.div.node().getBoundingClientRect().width;
+        this.chartHeight = this.div.node().getBoundingClientRect().height;
+
+        // small chart size
+        this.elemWidth  = (this.chartWidth - this.margins.left - this.margins.right - (this.nCharts-1)*this.spaceBetween) / this.nCharts;
+        this.elemHeight = this.chartHeight - this.margins.top  - this.margins.bottom ;
+        
+        // create the svg if not defined
+        if(typeof this.svg === "undefined")
+            this.svg = this.div.append('svg');
+        
+        // update the sizes
+        this.svg.attr("width", this.chartWidth)
+                .attr("height", this.chartHeight);
+        
+        // create the chart group if not defined
+        if(typeof this.cht === "undefined")
+            this.cht = this.svg.append('g');
+        
+        // update the sizes
+        this.cht.attr("transform", "translate(" + this.margins.left + "," + this.margins.top + ")");        
+    }
+
+    private _buildData(json: any) 
+    {
+        // this scope
+        var that = this;
+
+        // sort features
+        var features = json.features.sort(function (x: any, y: any) {
+            return d3.ascending(x.rank, y.rank);
+        });
+
+        // get time keys
+        this.timeRes = Object.keys(features[features.length - 1]["resolutions"]);
+        // remove ALL key
+        this.timeRes.splice(this.timeRes.indexOf("ALL"), 1);
+
+        // build data
+        this.data = {};
+
+        // ranges definition
+        this.xRange = [Infinity, -Infinity];
+        this.yRange = [Infinity, -Infinity];
+
+        // mapping
+        this.timeRes.forEach(function (res: string) {
+            that.data[res] = features.map(function (f: any) 
+            {
+                // current resolution
+                var resolution = res;
+                // rank computation
+                var fnRank  = f.resolutions[resolution].fnRank;
+                var maxRank = f.resolutions[resolution].fnRank;
+                var sigRank = f.resolutions[resolution].fnRank;
+                
+                // x and y values for the scatter plot
+                var x = Math.sqrt(maxRank * maxRank + fnRank * fnRank + sigRank * sigRank);
+                var y = f.rank;
+
+                that.xRange[0] = Math.min(that.xRange[0], x);
+                that.xRange[1] = Math.max(that.xRange[1], x);
+
+                that.yRange[0] = Math.min(that.yRange[0], y);
+                that.yRange[1] = Math.max(that.yRange[1], y);
+
+                return { x: x, y: y };
+            });
         });
     }
 
-    private _buildChart() {
+    private _buildAxis()
+    {
+        // this scope
         var that = this;
-        var isSearch = false;
 
-        this.div = d3.select(this.element.nativeElement);
-
-        this.chartWidth  = this.div.node().getBoundingClientRect().width;
-        this.chartWidth -= this.margins.left + this.margins.right;
-        this.chartHeight  = this.div.node().getBoundingClientRect().height;
-        this.chartHeight -= this.margins.top + this.margins.bottom;
-
-        // scale
-        this.xScale = d3.scaleLinear().domain(this.xRange).range([0,this.chartWidth / 3]);
-        this.yScale = d3.scaleLinear().domain(this.yRange).range([this.chartHeight,0]);
+        // scale definition
+        this.xScale = d3.scaleLinear().domain(this.xRange).range([0,  this.elemWidth]);
+        this.yScale = d3.scaleLinear().domain(this.yRange).range([this.elemHeight, 0]);
 
         // axis
         this.xAxis = d3.axisBottom(this.xScale);
         this.yAxis = d3.axisLeft(this.yScale);
 
-        var numTicks = isSearch?5:5;
+        // number of ticks
+        var numTicks = this.isSearch ? 5 : 5;
+        
+        // x axis
         this.xAxis
             .ticks(numTicks)
-            .tickSize( this.chartHeight )
+            .tickSize(this.elemHeight)
             .tickFormat(d3.format(".1f"));
 
-        var yFormat = !isSearch?d3.format(".1f"):d3.format("f");
+        // y format 
+        var yFormat = !this.isSearch ? d3.format(".1f") : d3.format(".1f");
+        
+        // y axis
         this.yAxis
             .ticks(numTicks)
-            .tickSize(-this.chartWidth * this.timeRes.length, 0)
+            .tickSize(-this.chartWidth + this.margins.right + this.margins.left, 0)
             .tickFormat(yFormat);
 
-        this.div.select('svg').remove();
+        // data join
+        var xaxis = this.cht.selectAll(".x.axis")
+            .data(this.isSearch ? ['search'] : this.timeRes);
 
-        this.svg = this.div.append('svg')
-            .attr("width",  this.chartWidth)
-            .attr("height", this.chartHeight)
-            .append("g")
-            .attr("transform", "translate(" + this.margins.left + "," + this.margins.top + ")");
-
-        this.svg.selectAll(".x.axis")
-            .data(this.timeRes)
-            .enter().append("g")
-            .attr("class", "x axis")
-            .attr("transform", function(d: any, i: any) {
-                var pos = isSearch?0:i;
-                return "translate(" + (pos * that.chartWidth / that.timeRes.length) + "," + 0 + ")";
-            })
-            .each(function(d: any) {
-                that.xScale.domain(that.xRange);
-                d3.select(this).call(that.xAxis);
-            });
-
-        this.svg.selectAll(".y.axis")
-            .data(["func"])
+        // enter
+        var xEnter = xaxis
             .enter()
-            .append("g")
-            .attr("class", "y axis")
-            .attr("transform", function(d: any, i: any) {
-                var pos = isSearch?0:i;
-                return "translate("+ (pos*(that.chartWidth / that.timeRes.length)) +", 0)";
+            .append("g");
+
+        // merge
+        xaxis
+            .merge(xEnter)
+            .attr("class", "x axis")
+            .attr("transform", (d: any, i: any) => {
+                var pos = this.isSearch ? 0 : i;
+                return "translate(" + (pos * (this.elemWidth + this.spaceBetween)) + ", 0)";
             })
             .each(function() {
-                that.yScale.domain(that.yRange);
+                d3.select(this).call(that.xAxis);
+            });
+        
+        // exit
+        xaxis.exit().remove();
+
+        // join
+        var yaxis = this.cht.selectAll(".y.axis")
+            .data(["func"]);
+        
+        // enter
+        var yEnter = yaxis
+            .enter()
+            .append("g");
+
+        // merge
+        yaxis
+            .merge(yEnter)
+            .attr("class", "y axis")
+            .attr("transform", (d: any, i: any) => {
+                var pos = this.isSearch ? 0 : i;
+                return "translate(" + (pos * (this.elemWidth + this.spaceBetween)) + ", 0)";
+            })
+            .each(function() {
                 d3.select(this).call(that.yAxis);
             });
 
-        this.svg.selectAll('.cell')
-            .data(this.timeRes)
+        // exit
+        yaxis.exit().remove();
+    }
+
+    private _buildChart()
+    {
+        // this scope
+        var that = this;
+
+        // update
+        var cells = this.cht.selectAll('.cell')
+            .data(this.isSearch ? ['search'] : this.timeRes);
+        
+        // enter
+        var cellsEnter = cells
             .enter()
-            .append('g')
+            .append('g');
+        
+        // merge
+        cells
+            .merge(cellsEnter)
             .attr('class', 'cell')
-            .attr('transform', function(d: any, i: any) {
-                var pos = isSearch?0:i;
-                return "translate(" + (pos*(that.chartWidth / that.timeRes.length)) + ", 0)";
+            .attr('transform', function (d: any, i: any) {
+                var pos = this.isSearch ? 0 : i;
+                return "translate(" + (pos * (that.elemWidth + that.spaceBetween)) + ", 0)";
             })
-            .each(function(d: any) {
+            .each(function (d: any) 
+            {
+                // get cell and data
                 var cell = d3.select(this);
                 var data = that.data[d];
+                
+                // matrix frame join
+                var frames = cell.selectAll('.frame')
+                    .data(['frame']);
+                
+                // frames enter
+                var frameEnter = frames
+                    .enter()
+                    .append('rect');
 
-                cell.append("rect")
+                // merge
+                frames
+                    .merge(frameEnter)
                     .attr("class", "frame")
                     .attr("x", 0)
                     .attr("y", 0)
-                    .attr("width",  that.chartWidth / that.timeRes.length)
-                    .attr("height", that.chartHeight );
+                    .attr("width", that.elemWidth)
+                    .attr("height", that.elemHeight)                    
+                
+                // exit
+                frames.exit().remove();
 
+                // circles join
                 var circles = cell.selectAll("circle")
                     .data(data);
 
-                circles.enter().append("circle")
-                        .attr("cx", function(d: any) { return that.xScale(d.x); })
-                        .attr("cy", function(d: any) { return that.yScale(d.y); })
-                        .attr("r", 4)
-                        .style("fill", function(d: any) { return "blue"; });
+                // enter
+                var circlesEnter = circles
+                    .enter()
+                    .append("circle");
 
+                // merge
+                circles
+                    .merge(circlesEnter)                
+                    .attr("cx", function (d: any) { return that.xScale(d.x); })
+                    .attr("cy", function (d: any) { return that.yScale(d.y); })
+                    .attr("r", 4)
+                    .style("fill", function (d: any) { return "blue"; });
+
+                // exit
                 circles.exit().remove();
             });
 
-    }
-
-    private _resize(){
-        this._buildChart();    
+        // exit
+        cells.exit().remove();
     }
 }
